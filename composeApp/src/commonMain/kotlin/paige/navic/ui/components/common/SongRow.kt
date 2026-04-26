7592404798ad1f374af799374568dda16315ce12
Katsuki <katsuki.amatsuyu@yandex.ru>
2026-04-26 17:42:48 +0300

    fix: icons off-center because of insufficient SongRow width

--- composeApp/src/commonMain/kotlin/paige/navic/ui/components/common/SongRow.kt
+++ composeApp/src/commonMain/kotlin/paige/navic/ui/components/common/SongRow.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.material3.CircularProgressIndicator
@@ -19,6 +20,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.kyant.capsule.ContinuousRoundedRectangle
 import navic.composeapp.generated.resources.Res
@@ -79,10 +81,12 @@ fun SongRow(
 	val canPlay = isOnline || isDownloaded
 
 	ListItem(
-		modifier = modifier.combinedClickable (
-			onClick = onClick,
-			onLongClick = onLongClick
-		),
+		modifier = modifier
+			.width((LocalConfiguration.current.screenWidthDp * 0.875f).dp)
+			.combinedClickable (
+				onClick = onClick,
+				onLongClick = onLongClick
+			),
 		headlineContent = {
 			Text(song.title)
 		},
@@ -95,7 +99,7 @@ fun SongRow(
 					append(" • ")
 					append(song.year ?: stringResource(Res.string.info_unknown_year))
 				},
-				maxLines = 1
+				maxLines = 2
 			)
 		},
 		leadingContent = {
@@ -106,7 +110,7 @@ fun SongRow(
 			)
 		},
 		trailingContent = {
-			Row(verticalAlignment = Alignment.CenterVertically) {
+			Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier.height(83.dp)) {
 				if (!canPlay) {
 					Icon(
 						Icons.Outlined.Offline,
